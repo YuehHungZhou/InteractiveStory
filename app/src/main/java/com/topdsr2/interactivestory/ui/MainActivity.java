ea3b58dad1493c9cb60de905f17d08f259ad1dcd
topdsr2 <topdsr22@hotmail.com>
2019-01-19 08:33:11 +0800

    Initial commit

---
+++ app/src/main/java/com/topdsr2/interactivestory/ui/MainActivity.java
@@ -0,0 +1,52 @@
+package com.topdsr2.interactivestory.ui;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.topdsr2.interactivestory.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private EditText nameField;
+    private Button   starButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameField=(EditText)findViewById(R.id.nameEditText);
+        starButton=(Button)findViewById(R.id.starButton);
+
+        starButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameField.getText().toString();
+                //Toast.makeText(MainActivity.this,name,Toast.LENGTH_LONG).show();
+                starStory(name);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        nameField.setText("");
+    }
+
+    private void starStory(String name) {
+        Intent intent=new Intent(this,StoryActivity.class);
+        Resources resources=getResources();
+        String key = resources.getString(R.string.key_name);
+        intent.putExtra(key,name);
+        startActivity(intent);
+
+    }
+}
